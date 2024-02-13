19e6480a053ea6386b72b967410439e00d7e2c9c
PlasmaZen <31518912+PlasmaZen@users.noreply.github.com>
2024-02-13 09:35:15 -0700

    Add files via upload

--- romantoint.py
+++ romantoint.py
@@ -1,6 +1,10 @@
 def romanToInt(s: str) -> int:
     numeral_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     numeral_list = [i for i in s]
-    print(numeral_list)
+    answer = 0
+    # i need to go through the list and convert each value to an integer with the dictionary
+    for i in len(numeral_list):
+        if i in numeral_values:
+            answer += i
 
 print(romanToInt("III"))
